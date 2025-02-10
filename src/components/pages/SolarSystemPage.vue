13c53a8710f6cf278d2f5f7e8fbced5d8f277b12
Alessandro Sclafani <scla.alessandro@gmail.com>
2025-02-10 15:00:25 +0100

    aggiunto autoplay allo slider, migliorato responsive e corretti bug al primo avvio di pagina

--- src/components/pages/SolarSystemPage.vue
+++ src/components/pages/SolarSystemPage.vue
@@ -1,14 +1,18 @@
 <script>
-import {store} from '../../store';
+import {store, api} from '../../store';
 import Planet from '../minor_components/planet.vue'
 import { Swiper, SwiperSlide } from 'swiper/vue';
 import 'swiper/css';
-import { EffectCoverflow } from 'swiper/modules';
+import { EffectCoverflow, Autoplay } from 'swiper/modules';
+import axios from 'axios';
+
 export default {
   data() {
     return{
         store,
-        modules: [EffectCoverflow]
+        api,
+        modules: [EffectCoverflow, Autoplay],
+       
     }
   },
   components: {
@@ -16,16 +20,22 @@ export default {
     Swiper,
     SwiperSlide,
   },
+  methods: {
+    
+  },
+  mounted(){
+    this.api.getAllPlanets();
+  },
 }
 </script>
 
 <template>
     <div class="universe">
-        <section id="solar-system" class="d-flex align-items-center">
+        <section id="solar-system" class="d-flex align-items-center" v-show="api.showCarousel">
             <!-- <div v-for="planet, index in store.allPlanets" :key="index" :id="'orbit-' + index" class="orbit-generic">
                 <Planet :name="planet.name" :image="planet.img" />
             </div> -->
-            <Swiper class="custom-swiper-classes"
+            <Swiper class="custom-swiper-classes" 
             :modules="modules"
             :slides-per-view="1" 
             :breakpoints="{
@@ -36,18 +46,31 @@ export default {
                     slidesPerView: 5
                 }
             }"
+            :autoplay="{
+                delay: 5000,
+                pauseOnMouseEnter: true,
+            }"
             :effect="'coverflow'"
             :grabCursor="true"
             :centeredSlides="true"
             :coverflow-effect="{rotate: 0, stretch: -100, depth: 200, modifier: 1.5, scale: 0.8, slideShadows: false }"
+            :initialSlide="3"
+            :center-insufficient-slides="true"
+            :loop="true"
             :observer="true"
             :observe-parents="true"
-            :initialSlide="4"
-            :center-insufficient-slides="true"
-            :loop="true"> 
+            :speed="800"> 
             <!-- observer e observe-parents sono serviti perchè l'effetto coverflow non veniva renderizzato al primo reload della pagina, finchè non si interagiva con il carosello -->
-            <SwiperSlide class="custom-class-slide" v-for="planet, index in store.allPlanets" :key="index"><Planet :name="planet.name" :image="planet.img"/></SwiperSlide>
+                <SwiperSlide class="custom-class-slide" v-for="planet, index in store.allPlanets" :key="index">
+                    <Planet :planetHover="planet.name" :image="store.allPlanets[index].img"/>
+                    
+                </SwiperSlide>
             </Swiper>
+            <transition >
+                        <div id="info" v-if="store.showPlanet">
+                            {{ store.planetInfo }}
+                        </div>
+                    </transition>
         </section>
     </div>
 </template>
@@ -62,13 +85,37 @@ export default {
     height: calc(100vh - 4rem);
     position:relative;
     overflow:hidden;
+    #info{
+        position:absolute;
+        left:50%;
+        top:80%;
+        transform:translateX(-50%);
+        min-width:300px;
+        min-height:100px;
+        background-color:rgba(255, 255, 255, 0.158);
+        color:White;
+        border-radius:10px;
+        text-align:center;
+    }
     .custom-swiper-classes{
         height:100%;
         top:10%;
     }
 }
 
+.v-enter-active,
+.v-leave-active {
+  transition: all 0.5s ease-in-out;
+}
 
+.v-enter-from{
+    opacity: 0;
+    transform:translate(-50%, -30px);
+}
+.v-leave-to {
+    opacity: 0;
+    transform:translate(-50%, -30px);
+}
 
 // .info{
 //     min-width:20px;
