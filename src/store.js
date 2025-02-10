2daa1ab34cce1fc68b594f881b57db3d8561c93d
Alessandro Sclafani <scla.alessandro@gmail.com>
2025-02-10 14:58:58 +0100

    invertito ordine array, creata variabile api che conterrà solo le variabili e funzioni relative alle chiamate api

--- src/store.js
+++ src/store.js
@@ -1,44 +1,62 @@
 import {reactive} from 'vue';
+import axios from 'axios';
 
 export const store = reactive ({
     allPlanets:[
         {
-            name: 'Nettuno',
-            img: '/neptune.png'
-        },
-        {
-            name: 'Urano',
-            img: '/uranus.png'
-        },
-        {
-            name: 'Saturno',
-            img: '/saturn.png'
-        },
-        {
-            name: 'Giove',
-            img: '/jupiter.png'
-        },
-        {
-            name: 'Marte',
-            img: '/mars.png'
-        },
-        {
-            name: 'Terra',
-            img: '/earth.png'
-        },
-        {
-            name: 'Venere',
-            img: '/venus.png'
+            name: 'Sole',
+            img: '/sun.png'
         },
         {
             name: 'Mercurio',
             img: '/mercury.png'
         },
         {
-            name: 'Sole',
-            img: '/sun.png'
+            name: 'Venere',
+            img: '/venus.png'
+        },
+        {
+            name: 'Terra',
+            img: '/earth.png'
+        },
+        {
+            name: 'Marte',
+            img: '/mars.png'
+        }, 
+        {
+            name: 'Giove',
+            img: '/jupiter.png'
+        },
+        {
+            name: 'Saturno',
+            img: '/saturn.png'
+        },
+        {
+            name: 'Urano',
+            img: '/uranus.png'
+        },
+        {
+            name: 'Nettuno',
+            img: '/neptune.png'
         },
     ],
     planetInfo:'',
     showPlanet:false,
+})
+
+export const api = reactive ({
+    showCarousel: false, // necessario per non buggare il carousel, visto che riceveva i dati da una chiamata api
+    storedPlanets: [],
+    getAllPlanets(){
+        axios.get("http://127.0.0.1:8000/api/planets")
+        .then((resp)=> {
+            // console.log(resp.data);
+            this.storedPlanets = resp.data;
+            console.log(this.storedPlanets);
+            this.showCarousel = true;
+        })
+        .catch((error)=> {
+            console.error("Errore nella richiesta dei pianeti", error);
+        })
+    }
 })
