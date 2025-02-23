8f31575f1a6e7ca51a0fb583b2aaa414122aba29
Alessandro Sclafani <scla.alessandro@gmail.com>
2025-02-23 18:00:20 +0100

    aggiunto component meteore

--- src/components/pages/AboutMePage.vue
+++ src/components/pages/AboutMePage.vue
@@ -1,21 +1,33 @@
 <script>
+import Meteors from '../minor_components/Meteors.vue'
+
 export default {
   data() {
     
-  }
+  },
+  components: {
+    Meteors
+  },
 }
 </script>
 
 <template>
   <main>
+    <Meteors> 
         <section class="about-me">
             Alessandro Sclafani
         </section>
+    </Meteors>   
   </main>
 </template>
 
 <style lang="scss" scoped>
-
+  main{
+    width:100%;
+    height: calc(100vh - 4rem);
+   
+    
+  }
 
 
 </style>
